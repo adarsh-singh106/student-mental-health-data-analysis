a576f7615ce7c2aa7acecf94a02f2700ac10abff
Adarsh Singh <a1d0arsh6@gmail.com>
2026-08-27 23:55:09 +0530

    Add Warning Only When negative hours in physical hours are corrected

--- src/mental_health/data/cleaning.py
+++ src/mental_health/data/cleaning.py
@@ -11,6 +11,9 @@
     """
 
 import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
 
 def remove_duplicates(raw_df:pd.DataFrame)-> pd.DataFrame:
     """Removes all duplicate rows from DataFrame"""
@@ -24,7 +27,9 @@ def fix_physical_activity_hours(raw_df:pd.DataFrame)->pd.DataFrame:
     """Clips negative physical activity values to zero and records which rows were corrected."""
     df = raw_df.copy()
     
-
+    negative_hours = df[df['Physical_Activity_Hours'] < 0].shape[0]
+    if negative_hours > 0: # there were some rows with negative hours , log only then
+        logger.warning(f"Total Number of Rows Corrected in Column Physical_Activity_Hours : {negative_hours}")
     # Clip the Negative Values
     df['Physical_Activity_Hours'] = df['Physical_Activity_Hours'].clip(lower=0)
 
